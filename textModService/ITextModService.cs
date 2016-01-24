f377646158ac941ec148bb9047f4a777b01365f1
madflytom <madflytom@gmail.com>
2016-01-24 10:00:41 -0500

    Initial Commit

---
+++ textModService/ITextModService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace textModService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITextModService
+    {
+
+        [OperationContract]
+        string ChangeTest(string text);
+
+        
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class SomeText
+    {
+        [DataMember]
+        public string newText { get; set; }
+
+    }
+}
